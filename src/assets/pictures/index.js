664efd4ec403b49de8ea198f2f36326af3de2592
Sylvain T <sylvaintagnabou@gmail.com>
2023-05-26 16:59:02 +0000

    (add): assets

---
+++ src/assets/pictures/index.js
@@ -0,0 +1,29 @@
+import home from "./home.png";
+import ethColored from "./eth-colored.png";
+import ethPurple from "./eth-purple.png";
+import ethRainbow from "./eth-rainbow.png";
+import richEtherscanLogo from "./RichEtherscanLogo.png";
+import richEtherscan from "./richscan.png";
+import trueNorth from "./trueNorth.png";
+import alchemy from "./alchemy.png";
+import mapLight from "./map-light.png";
+import mapDark from "./map.png";
+import Error from "./404.png";
+import Copy from "./icons/copy.svg";
+import Tick from "./icons/tick.svg";
+
+export {
+  home,
+  ethColored,
+  ethPurple,
+  ethRainbow,
+  richEtherscanLogo,
+  richEtherscan,
+  trueNorth,
+  alchemy,
+  mapLight,
+  mapDark,
+  Error,
+  Copy,
+  Tick,
+};
