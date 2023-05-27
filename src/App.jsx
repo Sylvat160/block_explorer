bd4575cbab66e435ea1848da16af1b844b6a7118
Sylvain T <sylvaintagnabou@gmail.com>
2023-05-27 23:12:12 +0000

    (done): Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Navbar, Footer } from "./components";
 
 function App() {
-  
-  return (<>
-    <div className="App" > 
+  return (
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
       <Navbar />
 
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        {/* <Route path="/about" element={<h1>About</h1>} /> */}
+        <Route path="/about" element={<h1>About</h1>} />
       </Routes>
 
       <Footer />
     </div>
-  </>);
+  );
 }
 
 export default App;
