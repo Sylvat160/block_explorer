a6962501b8f3b57c0a1c053daa8a2045de86b62f
Sylvain T <sylvaintagnabou@gmail.com>
2023-05-30 13:00:31 +0000

    (add) : assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,10 +3,20 @@ import { ethPurple } from './pictures'
 import xplorer from './x-plorer.png'
 import wavelight from './waves-light.svg'
 import search from './search-icon.jpg'
+import blackether from './blackether.svg'
+import globe from './globe.svg'
+import time from './time.svg'
+import tx from './tx.svg'
+
+
 export {
     gas,
     ethPurple,
     xplorer,
     wavelight,
-    search
+    search,
+    blackether,
+    globe,
+    time,
+    tx
 }
